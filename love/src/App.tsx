d4325001a13612253f7ad7d84059c7e2e499a4a0
isaac <imisaacgonzalez@gmail.com>
2024-05-15 20:57:42 -0400

    Changes: Set-up routes in Navbar + App

--- love/src/App.tsx
+++ love/src/App.tsx
@@ -1,6 +1,12 @@
 import {Route, Routes} from "react-router-dom"
 import Navbar from "./components/navbar/Navbar"
 import Home from "./components/home/Home"
+import Photos from "./components/photos/Photos"
+import Locations from "./components/locations/Locations"
+import List from "./components/list/List"
+import Login from "./components/login/Login"
+import Recommended from "./components/recommended/Recommended"
+import Archive from "./components/archive/Archive"
 import BadRoute from "./components/404/BadRoute"
 import "./App.css"
 export default function App() {
@@ -9,6 +15,12 @@ export default function App() {
       <Navbar/>
     <Routes>
       <Route path="/" element={<Home/>}/>
+      <Route path="/photos" element={<Photos/>}/>
+      <Route path="/locations" element={<Locations/>}/>
+      <Route path="/list" element={<List/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/recommended" element={<Recommended/>}/>
+      <Route path="/archive" element={<Archive />}/>
       <Route path="*" element={<BadRoute/>}/>
     </Routes>
     </div>
--- love/src/components/navbar/Navbar.tsx
+++ love/src/components/navbar/Navbar.tsx
@@ -5,15 +5,15 @@ export default function Navbar() {
     <div className="navbar">
         <div className="navbar__container">
             <ul className="navbar__list">
-                <li className="navbar__item">PHOTOS</li>
-                <li className="navbar__item">IDEAS</li>
-                <li className="navbar__item">LIST</li>
-                <li className="navbar__item">
-                    <img className="navbar__itemImg" src="https://www.onlygfx.com/wp-content/uploads/2021/02/6-pixel-heart-6.png" alt="logo" />
-                </li>
+                <Link className="navbar__item" to="/photos">PHOTOS</Link>
+                <Link className="navbar__item" to="/locations">LOCATIONS</Link>
+                <Link className="navbar__item" to="/list">LIST</Link>
+                <Link to="/" className="navbar__item">
+                  <img className="navbar__itemImg" src="https://www.onlygfx.com/wp-content/uploads/2021/02/6-pixel-heart-6.png" alt="logo" />
+                </Link>
                 <Link to="/login" className="navbar__item">LOGIN</Link>
-                <li className="navbar__item">RECOMMENED</li>
-                <li className="navbar__item">ARCHIVE</li>
+                <Link className="navbar__item" to="/recommended">RECOMMENDED</Link>
+                <Link className="navbar__item" to="/archive">ARCHIVE</Link>
             </ul>
         </div>
     </div>
