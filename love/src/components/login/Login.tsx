2d1cf8d93552333ca7bd61643482d4fe855cbe6b
isaac <imisaacgonzalez@gmail.com>
2024-05-15 21:42:11 -0400

    Changes: Basic Login HTML built (awaiting Firebase)

--- love/src/components/login/Login.tsx
+++ love/src/components/login/Login.tsx
@@ -1,8 +1,14 @@
-
+import "./Login.scss";
 export default function Login() {
   return (
-    <div>
-      Login
+    <div className="login">
+      <div className="login__container">
+        <div className="login__title">LOGIN</div>
+        <input className="login__inputUser" type="text" />
+        <input className="login__inputPasswrd" type="password" />
+        <button className="login__btnLogin">LOGIN</button>
+        <button className="login__btnRegister">CREATE NEW USER</button>
+      </div>
     </div>
   )
 }
